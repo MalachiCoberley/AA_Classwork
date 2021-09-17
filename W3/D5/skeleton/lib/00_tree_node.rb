7d7044a6debd5d0207f62460af233f1a37ede965
Ambrogino Giusti <ambroginogiusti@gmail.com>
2021-09-17 15:26:02 -0700

    Add init and parent methods

--- W3/D5/skeleton/lib/00_tree_node.rb
+++ W3/D5/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,16 @@
 class PolyTreeNode
 
+  attr_reader :parent, :children, :value
+
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent=(value)
+    parent = value
+    parent.children.push(self) if !parent.nil?
+  end
+
 end
