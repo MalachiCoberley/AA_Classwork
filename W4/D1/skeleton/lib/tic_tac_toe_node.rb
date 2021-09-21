4749b7ec05546f86b20247c7f534c7fb779e58dd
Ambrogino Giusti <ambroginogiusti@gmail.com>
2021-09-20 17:17:11 -0700

    Work on init

--- W4/D1/skeleton/lib/tic_tac_toe_node.rb
+++ W4/D1/skeleton/lib/tic_tac_toe_node.rb
@@ -1,13 +1,35 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+
+  #  :board, :next_mover_mark, :prev_move_pos
+
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
+  end
+
+  def board
+    @board
+  end
+
+  def children
+    (0...3).each do |i|
+      (0...3).each do |j|
+        if board[i][j].empty?
+          TicTacToeNode.new(@board, @next_mover_mark, [i, j])
+        end
+      end
+    end
   end
 
   def losing_node?(evaluator)
+
   end
 
   def winning_node?(evaluator)
+
   end
 
   # This method generates an array of all moves that can be made after
@@ -15,3 +37,6 @@ class TicTacToeNode
   def children
   end
 end
+
+# new_var = TicTacToeNode.new(Board.new, :x)
+# p new_var
