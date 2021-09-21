ccd5cedcb5340d491f0962ee62c6f6c0a9db96e3
MalachiCoberley <malachicoberley@gmail.com>
2021-09-21 14:32:25 -0700

    Add Manager and Employee classes

---
+++ W4/D2/class_inheritance.rb
@@ -0,0 +1,50 @@
+require "byebug"
+
+class Employee
+
+  attr_accessor :name, :title, :salary, :boss
+
+  def initialize(name, title, salary, boss=nil)
+    @name = name
+    @title = title
+    @salary = salary
+    @boss = boss
+  end
+
+  def bonus(multiplier)
+    bonus = @salary * multiplier
+  end
+
+end
+
+class Manager < Employee
+
+  def initialize(name, title, salary, boss, employees)
+    super(name, title, salary, boss)
+    @employees = employees
+  end
+
+  def bonus(multiplier)
+    # debugger
+    total_salary = 0
+    @employees.each do |employee|
+      if employee.is_a?(Manager)
+      total_salary += employee.salary
+    end
+    total_salary * multiplier
+  end
+
+end
+
+
+shawna = Employee.new("Shawna", "TA", 12000)
+david = Employee.new("David", "TA", 10000)
+darren = Manager.new("Darren", "TA Manager", 78000, nil, [shawna, david])
+ned = Manager.new("Ned", "Founder", 1000000, nil, [darren])
+shawna.boss = darren
+david.boss = darren
+darren.boss = ned
+
+p ned.bonus(5) # => 500_000
+p darren.bonus(4) # => 88_000
+p david.bonus(3) # => 30_000
