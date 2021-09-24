b4a6cbdf0d50df1948c4327c806cc971f36da32e
Matthew Lese <matthewjlese@gmail.com>
2021-09-24 15:37:12 -0700

    completes anagrams exercise

--- W4/D5/anagrams.rb
+++ W4/D5/anagrams.rb
@@ -14,5 +14,17 @@ def second_anagram?(str1, str2)
   str2_array.join("") == ""
 end
 
+def third_anagram?(str1, str2)
+  str1.split("").sort == str2.split("").sort
+end
 
-p second_anagram?("cat", "toooooooooooc")
+def fourth_anagram?(str1, str2)
+  hash1 = Hash.new(0)
+  hash2 = Hash.new(0)
+  str1.each_char { |char| hash1[char] += 1 }
+  str2.each_char { |char| hash2[char] += 1 }
+  hash1 == hash2
+end
+
+p fourth_anagram?("cat", "toooooooooooc")
+p fourth_anagram?("cat", "tac")
