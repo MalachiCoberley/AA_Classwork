db1d129a9fbe207608a1260c3c2bbbc7e35bebb1
Matthew Lese <matthewjlese@gmail.com>
2021-09-24 16:56:58 -0700

    end of w4d5 work

--- W4/D5/two_sum.rb
+++ W4/D5/two_sum.rb
@@ -4,20 +4,26 @@ def bad_two_sum?(arr, target_sum)
   false
 end
 
-# def ok_two_sum?(arr, target_sum)
-#   sorted = arr.sort.select { |el| el < target_sum }
-#   sorted.each do |el|
-#     diff = target_sum - el
-#     b = sorted.bsearch { |x| x == diff}
-#     puts b
-#     puts el
-#     return true if b != nil
+def ok_two_sum?(arr, target_sum)
+  sorted = arr.sort.select { |el| el < target_sum }
+  sorted.each do |el|
+    diff = target_sum - el
+    b = sorted.bsearch { |x| x == diff }
+    return true if b != nil
+  end
+  false
+end
+
+# def two_sum?(arr, target_sum)
+#   hash = Hash.new(0)
+#   arr.each do |el|
+#     return true if hash[target_sum - el] > 0
+#     hash[el] += 1
 #   end
 #   false
 # end
 
-
-
-arr = [0, 1, 5, 7]
-p ok_two_sum?(arr, 6) # => should be true
-p ok_two_sum?(arr, 10) # => should be false
+arr = [0, 1, 5, 7, 3, 4, 12]
+p ok_two_sum?(arr, 17) # => should be true
+p ok_two_sum?(arr, 10) # => should be true
+p ok_two_sum?(arr, 24) # => should be false, watch for doubles
