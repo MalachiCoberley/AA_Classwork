c4ff0b88b05f91e3309e6c12fd4561842eb3a6e0
MalachiCoberley <malachicoberley@gmail.com>
2021-09-27 16:04:37 -0700

    Create Hash functions

--- W5/D1/skeleton/lib/p02_hashing.rb
+++ W5/D1/skeleton/lib/p02_hashing.rb
@@ -4,11 +4,21 @@ end
 
 class Array
   def hash
+    hash_sum = 0
+    self.each_with_index do |el, i|
+      hash_sum += (el + i).hash
+    end
+    hash_sum
   end
 end
 
 class String
   def hash
+    hash_sum = 0
+    self.each_char.with_index do |char, i|
+      hash_sum += (char.ord + i).hash
+    end
+    hash_sum
   end
 end
 
@@ -16,6 +26,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    hash_sum = 0
+    self.each do |k, v|
+      hash_sum += (char.ord + i).hash
+    end
+    hash_sum
   end
 end
