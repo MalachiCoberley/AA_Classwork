9d1b55029e0911ca2b92170775bafeb2c0203df1
MalachiCoberley <malachicoberley@gmail.com>
2021-09-29 16:09:00 -0700

    Add QuestionLike class methods for likes

--- W5/D3/aa_questions/questions.rb
+++ W5/D3/aa_questions/questions.rb
@@ -333,11 +333,32 @@ end
 class QuestionLike
 
   def self.likers_for_questions_id(question_id)
-
+    users = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+    SELECT
+      users.*
+    FROM
+      users
+    JOIN
+      question_likes
+    ON
+      users.id = question_likes.user_id
+    WHERE
+      question_likes.question_id = ?
+    SQL
+    users.map {|user| User.new(user)}
   end
 
   def self.num_likes_for_question_id(question_id)
     # dont just use question liker for question ID and count, do a SQL query
+    count_hash_arr = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+    SELECT
+      COUNT(*)
+    FROM
+      question_likes
+    WHERE
+      question_id = ?
+    SQL
+    count_hash_arr.first["COUNT(*)"]
   end
 
   def self.liked_questions_for_user_id(user_id)
