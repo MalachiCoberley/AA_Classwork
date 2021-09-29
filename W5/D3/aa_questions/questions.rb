34e79649b4ffb7a8c55a45a66f1a490c8083421a
MalachiCoberley <malachicoberley@gmail.com>
2021-09-29 13:33:40 -0700

    Create more methods

--- W5/D3/aa_questions/questions.rb
+++ W5/D3/aa_questions/questions.rb
@@ -32,9 +32,159 @@ class User
     User.new(user.first)
   end
 
+  def self.find_by_name(fname, lname)
+    user = QuestionsDatabase.instance.execute(<<-SQL, fname, lname)
+    SELECT
+      *
+    FROM
+      users
+    WHERE
+      fname = ?
+    AND
+      lname = ?
+    SQL
+
+    User.new(user.first)
+  end
+
   def initialize(options)
     @id = options['id']
     @fname = options['fname']
     @lname = options['lname']
   end
+
+  def authored_questions
+    #this will use Question::find_by_author_id
+  end
+
+  def authored_replies
+    #this will use Reply::find_by_user_id
+  end
+
+end
+
+#Start of Question Class ------------------------------------------
+
+class Question
+  attr_accessor :title, :body, :user_id
+
+  def self.all
+    data = QuestionsDatabase.instance.execute("SELECT * FROM questions")
+    data.map { |datum| Question.new(datum) }
+  end
+
+  def self.find_by_id(id)
+    question = QuestionsDatabase.instance.execute(<<-SQL, id)
+    SELECT
+      *
+    FROM
+      questions
+    WHERE
+      id = ?
+    SQL
+
+    Question.new(question.first)
+  end
+
+  def self.find_by_author_id(user_id)
+    question = QuestionsDatabase.instance.execute(<<-SQL, user_id)
+    SELECT
+     *
+    FROM
+      questions
+    WHERE
+      user_id = ?
+    SQL
+    Question.new(question.first)
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @title = options['title']
+    @body = options['body']
+    @user_id = options['user_id']
+  end
+
+  def author
+
+  end
+
+  def replies
+    #use Reply::find_by_question_id
+  end
+
+end
+
+
+#Start of Reply Class ------------------------------------------
+
+class Reply
+  attr_accessor :id, :subject_question_id, :reply_to_id, :user_id, :body
+
+  def self.all
+    data = QuestionsDatabase.instance.execute("SELECT * FROM replies")
+    data.map { |datum| Reply.new(datum) }
+  end
+
+  def self.find_by_id(id)
+    reply = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        replies
+      WHERE
+        id = ?
+    SQL
+
+    Reply.new(reply.first)
+  end
+
+  def self.find_by_user_id(user_id)
+    reply = QuestionsDatabase.instance.execute(<<-SQL, user_id)
+      SELECT
+        *
+      FROM
+        replies
+      WHERE
+        user_id = ?
+    SQL
+
+    Reply.new(reply.first)
+  end
+
+  def self.find_by_question_id(question_id)
+    replies = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+    SELECT
+      *
+    FROM
+      replies
+    WHERE
+      question_id = ?
+    SQL
+
+    replies.map { |reply| Reply.new(reply)}
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @fname = options['fname']
+    @lname = options['lname']
+  end
+
+  def author
+
+  end
+
+  def question
+
+  end
+
+  def parent_reply
+
+  end
+
+  def child_replies
+    # Only do child replies one-deep; don't find grandchild comments.
+  end
+
 end
