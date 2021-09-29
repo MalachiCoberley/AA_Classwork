0b5627c465cb7c50e3633847c3d504eb0a291dc2
Jean Youn Lee <jean.youn.lee@gmail.com>
2021-09-29 11:52:06 -0700

    Create question.rb with QuestionsDatabase class and start implementing User class

---
+++ W5/D3/aa_questions/questions.rb
@@ -0,0 +1,40 @@
+require 'sqlite3'
+require 'singleton'
+
+class QuestionsDatabase < SQLite3::Database
+  include Singleton
+
+  def initialize
+    super('questions.db')
+    self.type_translation = true
+    self.results_as_hash = true
+  end
+end
+
+class User
+  attr_accessor :id, :fname, :lname
+
+  def self.all
+    data = QuestionsDatabase.instance.execute("SELECT * FROM users")
+    data.map { |datum| User.new(datum) }
+  end
+
+  def self.find_by_id(id)
+    user = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        users
+      WHERE
+        id = ?
+    SQL
+
+    User.new(user.first)
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @fname = options['fname']
+    @lname = options['lname']
+  end
+end
