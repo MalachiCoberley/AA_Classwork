1590a486a7872ae5e53a3f150e3bad3336342559
MalachiCoberley <malachicoberley@gmail.com>
2021-10-12 12:22:27 -0700

    complete user authentication

---
+++ W7/D2/MusicApp/app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+  before_action :require_no_user!, only: %i(create new)
+
+  def create
+    user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+    if user
+      login_user!(user)
+      redirect_to albums_url
+    else
+        flash.now[:errors] = ["Invalid password and/or username"]
+        render :new
+    end
+  end
+
+  def new
+    render :new
+  end
+
+
+  def destroy
+    logout_user!
+    redirect_to new_session_url
+  end
+
+end
---
+++ W7/D2/MusicApp/app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  before_action :require_no_user!
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login_user!(@user)
+      redirect_to albums_url
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def show
+    render :show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
