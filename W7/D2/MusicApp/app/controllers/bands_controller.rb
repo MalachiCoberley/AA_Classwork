2de98675ec28e4160fe768f0013be8ff7bad231d
MalachiCoberley <malachicoberley@gmail.com>
2021-10-12 16:17:19 -0700

    Add Bands

---
+++ W7/D2/MusicApp/app/controllers/bands_controller.rb
@@ -0,0 +1,53 @@
+class BandsController < ApplicationController
+
+  def create
+    @band = Band.new(band_params)
+    if @band.save
+      redirect_to bands_url(@band)
+    else
+      flash.now[:errors] = @band.errors.full_messages
+      render :new
+    end
+  end
+
+  def index
+    @bands = Band.all
+      render :index
+  end
+
+  def new
+    @band = Band.new
+    render :new
+  end
+
+  def edit
+    @band = Band.find(params[:id])
+  end
+
+  def show
+    @band = Band.find(params[:id])
+    render :show
+  end
+
+  def update
+    @band = find(params[:id])
+    if @band.update_attributes(band_params)
+      redirect_to band_url(@band)
+    else
+      flash.now[:errors] = @band.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy
+    band = find(params[:id])
+    band.destroy
+    redirect_to bands_url
+  end
+
+  private
+
+  def band_params
+    params.require(:band).permit(:name)
+  end
+end
