72bf79acd87b215397ed806f91f2c9bd138e984a
MalachiCoberley <malachicoberley@gmail.com>
2021-10-12 18:06:30 -0700

    You can ady

--- W7/D2/MusicApp/app/controllers/application_controller.rb
+++ W7/D2/MusicApp/app/controllers/application_controller.rb
@@ -17,6 +17,7 @@ class ApplicationController < ActionController::Base
   def logout_user!
     current_user.reset_session_token!
     session[:session_token] = nil
+    @current_user = nil
   end
 
   def require_user!
