b70358bf1a31c1e27db178fca5d8b9089693344a
MalachiCoberley <malachicoberley@gmail.com>
2021-10-13 15:07:41 -0700

    User specs

---
+++ W7/D3/GoalApp/app/models/user.rb
@@ -0,0 +1,27 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  session_token   :string           not null
+#  password_digest :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+  attr_reader :password
+  before_validation :ensure_session_token
+  validates :username, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: {minimum: 6}
+  
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom::urlsafe_base64
+  end
+end
---
+++ W7/D3/GoalApp/spec/models/user_spec.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  session_token   :string           not null
+#  password_digest :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+require 'rails_helper'
+
+# session_token: "abc123", password_digest: "123abc"
+
+RSpec.describe User, type: :model do
+  subject(:malachi) {User.create!(username: "malachi", password: "abc123")}
+  it { should validate_presence_of(:username) }
+  it { should validate_uniqueness_of(:username) }
+  it { should validate_uniqueness_of(:session_token) }
+  it { should validate_presence_of(:password_digest) }
+  it { should validate_length_of(:password).is_at_least(6) }
+  
+end
