826b4e007489ac05e3158feba858d8303bec98b5
Jshin720 <jeffshin720@gmail.com>
2021-11-10 16:51:41 -0500

    First push of w11d3 finished setup'

---
+++ W11/D2/todos/frontend/components/app.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const app =() =>  {
+  return <h1> Todos - App </h1>
+}
+
+export default app;
--- W11/D2/todos/todo_redux.jsx
+++ W11/D2/todos/todo_redux.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom'
 import configureStore from './frontend/store/store'
 import { receiveTodo, receiveTodos, removeTodo } from './frontend/actions/todo_actions'
 import { receiveStep, receiveSteps, removeStep } from './frontend/actions/step_actions'
+import Root from './frontend/components/root'
+import rootReducer from './frontend/reducers/root_reducer'
 
 document.addEventListener("DOMContentLoaded", () => {
-  ReactDOM.render(<h1>Todos App</h1>, document.getElementById("root"))
+  const store = configureStore(rootReducer)
+  ReactDOM.render(<Root store={store} />, document.getElementById("root"))
   
   window.store = configureStore();
   window.receiveTodo = receiveTodo;
