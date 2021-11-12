def3bce0184725069adebbe80d45c6e485dc9daf
MalachiCoberley <malachicoberley@gmail.com>
2021-11-12 15:34:12 -0700

    Create Actions and Util

--- W11/D5/Pokedex/frontend/pokedex.jsx
+++ W11/D5/Pokedex/frontend/pokedex.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {receiveAllPokemon} from "./actions/pokemon_actions";
+import {fetchAllPokemon} from "./util/api_util";
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('root');
+  window.receiveAllPokemon = receiveAllPokemon;
+  window.fetchAllPokemon = fetchAllPokemon;
   ReactDOM.render(<h1>Pokedex</h1>, rootEl);
 });
