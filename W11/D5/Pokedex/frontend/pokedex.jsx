7197b2bc7da65787f5a70b5778473ea90672b99a
MalachiCoberley <malachicoberley@gmail.com>
2021-11-12 16:22:06 -0700

    Complete API endpoint and Redux Cycle

--- W11/D5/Pokedex/frontend/pokedex.jsx
+++ W11/D5/Pokedex/frontend/pokedex.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {receiveAllPokemon} from "./actions/pokemon_actions";
+import {receiveAllPokemon, requestAllPokemon} from "./actions/pokemon_actions";
+import configureStore from "./store/store";
 import {fetchAllPokemon} from "./util/api_util";
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('root');
+  const store = configureStore();
+  window.store = store;
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   window.receiveAllPokemon = receiveAllPokemon;
+  window.requestAllPokemon = requestAllPokemon;
   window.fetchAllPokemon = fetchAllPokemon;
   ReactDOM.render(<h1>Pokedex</h1>, rootEl);
 });
