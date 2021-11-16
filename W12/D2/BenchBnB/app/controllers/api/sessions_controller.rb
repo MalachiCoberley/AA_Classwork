b600e62d8ca0036847481523816868e648c9cf24
MalachiCoberley <malachicoberley@gmail.com>
2021-11-16 15:53:13 -0700

    Create Controllers

--- W12/D2/BenchBnB/app/controllers/api/sessions_controller.rb
+++ W12/D2/BenchBnB/app/controllers/api/sessions_controller.rb
@@ -1,2 +1,17 @@
 class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    if @user.nil?
+      render json: ["User does not exist"], status: 401
+    else
+      login!(@user)
+      render "api/users/show"
+    end
+  end
+
+  def destroy
+    logout!
+    render json: ["Logout successful"]
+  end
+  
 end
