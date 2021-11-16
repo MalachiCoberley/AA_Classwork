60f824b262b193f8eb16e82f93d4d6781fc2e398
your name here <your_email_hereroelliga@yahoo.com>
2021-11-16 14:27:01 -0800

    controllers

--- W12/D2/BenchBnB/app/controllers/api/users_controller.rb
+++ W12/D2/BenchBnB/app/controllers/api/users_controller.rb
@@ -1,2 +1,23 @@
 class Api::UsersController < ApplicationController
+
+
+    def create
+        @user = User.new(user_params);
+        if @user.save
+            login!(@user)
+            render :show
+        else
+            render json: @user.errors.full_messages, status: 401
+        end
+    end
+
+    def show
+        @user = user.find(params[:id])
+        render :show
+    end
+
+    private 
+    def user_params 
+        params.require(:user).permit(:username, :password)
+    end
 end
